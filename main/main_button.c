947d5694eefcd1a3e052e3d410194f0d1b87dff9
Christopher Mason <cmason@cmason.com>
2017-01-12 13:02:16 -0800

    Clean up button example.

--- main/main_button.c
+++ main/main_button.c
@@ -8,21 +8,16 @@
 volatile static bool led = 0;
 
 
-
 void IRAM_ATTR button_intr(void* arg) {
-    uint32_t status = GPIO.status;
-    if (status && 1 << GPIO_NUM_0) {
+    if (!gpio_get_level(GPIO_NUM_0)) {
+
+        for (volatile int i = 0; i < 10000; ++i) {}  // Cheesy debounce.
+
         if (!gpio_get_level(GPIO_NUM_0)) {
-
-            for (volatile int i = 0; i < 10000; ++i) {}  // Cheesy debounce.
-
-            if (!gpio_get_level(GPIO_NUM_0)) {
-                led = !led;
-                gpio_set_level(GPIO_NUM_5, led);
-            }
+            led = !led;
+            gpio_set_level(GPIO_NUM_5, led);
         }
     }
-    GPIO.status_w1tc = status;
 }
 
 
@@ -31,13 +26,14 @@ void app_main(void) {
     gpio_set_direction(GPIO_NUM_0, GPIO_MODE_INPUT);
 
     gpio_set_intr_type(GPIO_NUM_0, GPIO_INTR_NEGEDGE);
-    gpio_set_pull_mode(GPIO_NUM_0, GPIO_PULLUP_ONLY);
     gpio_intr_enable(GPIO_NUM_0);
 
-    gpio_isr_register(
-        button_intr,             // Interrupt handler.
-        NULL,                    // Argument to GPIO function.
-        ESP_INTR_FLAG_LEVEL2,    // Flags
-        NULL                     // Handle to interrupt registration.
+    gpio_install_isr_service(ESP_INTR_FLAG_LEVEL2);
+
+    gpio_isr_handler_add(
+        GPIO_NUM_0,
+        button_intr,
+        NULL
     );
+
 }
